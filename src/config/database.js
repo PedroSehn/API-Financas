2a2fa60c2d7aa228e8cc373bd0172820a46c8f9f
Pedro Sehn <pedrorsehn@hotmail.com>
2026-04-09 22:47:39 -0300

    debug2

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 console.log('DATABASE_URL recebida:', process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
