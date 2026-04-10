4136314909ac9e66fee694a9e7b6a2a39b779a8e
Pedro Sehn <pedrorsehn@hotmail.com>
2026-04-09 23:22:17 -0300

    debug7

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 
-console.log('DATABASE_URL recebida:', process.env.DATABASE_URL);
+console.log('PGHOST:', process.env.PGHOST);
+console.log('PGDATABASE:', process.env.PGDATABASE);
+console.log('PGUSER:', process.env.POSTGRES_USER);
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.POSTGRES_USER,
+  password: process.env.PGPASSWORD,
 });
 
 pool.connect()
-  .then(() => console.log('PostgreSQL conectado'))
-  .catch((err) => console.error('Erro ao conectar no banco:', err));
+    .then(() => console.log('PostgreSQL conectado'))
+    .catch((err) => console.error('Erro ao conectar no banco:', err));
 
 module.exports = pool;
